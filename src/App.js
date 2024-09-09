84b6a5d65747cc0e2d2eacf5c57ac154012a49fc
richTech <cartagenaemilio1117@gmail.com>
2024-09-09 01:14:55 -0400

    updated button

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
     const script = document.createElement('script');
     script.src = 'https://telegram.org/js/telegram-widget.js?22';
     script.async = true;
-    script.setAttribute('data-telegram-login', 'unigames_tg_bot'); // Replace with your bot username
+    script.setAttribute('data-telegram-login', 'yourbotusername'); // Replace with your bot username
     script.setAttribute('data-size', 'large');
     script.setAttribute('data-onauth', 'onTelegramAuth(user)');
     script.setAttribute('data-request-access', 'write');
@@ -54,6 +54,7 @@ function App() {
         <h1>Login with Telegram</h1>
         <p>Click the button below to log in with your Telegram account:</p>
         {/* The Telegram login button will be injected here by the widget script */}
+        <div id="telegram-login-button"></div>
       </header>
     </div>
   );
